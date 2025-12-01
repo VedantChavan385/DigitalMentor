4658ee8a94b17c7909a8498c9c0ffa818491b916
Vedant Chavan <vedantchavan385@gmail.com>
2025-12-01 21:37:21 +0530

    updated

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const schema = new mongoose.Schema({ name:String, email:{type:String, unique:true}, password:String, role:{type:String, enum:['mentor','mentee','admin'], default:'mentee'}, expertise:String, bio:String, avatar:String });
 schema.pre('save', async function(next){ if(!this.isModified('password')) return next(); this.password = await bcrypt.hash(this.password,10); next(); });
 schema.methods.matchPassword = function(p){ return require('bcrypt').compare(p, this.password); };
